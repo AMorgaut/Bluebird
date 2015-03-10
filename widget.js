6ed0f0a892d261b4a841e2353f22dd176dd63b4f
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2015-03-11 00:31:04 +0100

    Wakanda Bluebird package

---
+++ widget.js
@@ -0,0 +1,9 @@
+WAF.define('Bluebird', ['waf-core/widget'], function(widget) {
+	
+    var Bluebird = widget.create('Bluebird');
+
+    return Bluebird;
+
+});
+
+/* For more information, refer to http://doc.wakanda.org/Wakanda/help/Title/en/page3871.html */
